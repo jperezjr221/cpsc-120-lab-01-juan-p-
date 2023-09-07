28e8e104e463aac6a4621dca2b14261021617979
Juan Perez <jperezjr@csu.fullerton.edu>
2023-09-06 18:47:18 -0700

    Added a personalized header. My first git commit.

---
+++ hello.cc
@@ -0,0 +1,11 @@
+// Juan Perez
+// jperezjr@csu.fullerton.edu
+// @jperezjr221
+// Partners: 
+
+#include <iostream>
+
+int main(int argc, const char *argv[]) {
+  std::cout << "Hello World!\n";
+  return 0;
+}
